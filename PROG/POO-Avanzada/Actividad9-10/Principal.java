e8cd5fe25dbca71d733283120343f0e80c35d479
Pablon03 <60820789+Pablon03@users.noreply.github.com>
2022-03-22 10:18:28 +0100

    Update Principal.java

--- PROG/POO-Avanzada/Actividad9-10/Principal.java
+++ PROG/POO-Avanzada/Actividad9-10/Principal.java
@@ -1,7 +1,9 @@
 package Actividad9Estudiando;
+import java.util.*;
 
 public class Principal {
 
+	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		
 		Publicaciones listaPublicaciones[] = new Publicaciones[4];
@@ -10,7 +12,42 @@ public class Principal {
 		listaPublicaciones[2] = new Revistas("123456W", "Rompe la barrera del NO", 2010, 1);
 		listaPublicaciones[3] = new Revistas("68546P", "Vende como el mejor", 2020, 2);
 		
+		//Transformamos a libro y lo prestamos
+		Libros i = (Libros)listaPublicaciones[0];
+		i.prestar();
+		
+		//Mostramos el array
+		for (int j = 0; j < listaPublicaciones.length; j++) {
+			System.out.println(listaPublicaciones[j]);
+		}
+		
+		//¿Cuántos hay prestados?
+		System.out.println("Hay " + cuantosPrestados(listaPublicaciones) + " publicaciones prestadas"); 
+		
+		// Publicacion anterior A
+		int resultado = publicacionesAnterioresA(1990, listaPublicaciones);
+		System.out.println("Hay " + resultado + " publicaciones anteriores a " + 1990);
 		
 		
+	}
+	
+	public static int publicacionesAnterioresA(int anio, Publicaciones publicaciones[]) {
+		int sumaAnteriores = 0;
+		
+		for (int i = 0; i < publicaciones.length; i++) {
+			if (publicaciones[i].getAnioPublicacion() < anio) {
+				sumaAnteriores ++;
+			}
+		}
+		
+		return sumaAnteriores;
+	}
+	
+	public static int cuantosPrestados(Object[] listaPublicaciones) {
+		int prestados = 0;
+		for (int i = 0; i < listaPublicaciones.length; i++) {
+
+			
+		}
 	}
 }
