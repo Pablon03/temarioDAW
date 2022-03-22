9194708d42abbe792a087d3aeb6a76d1491bd987
Pablon03 <60820789+Pablon03@users.noreply.github.com>
2022-03-22 10:05:15 +0100

    Create Libros.java

---
+++ PROG/POO-Avanzada/Actividad9-10/Libros.java
@@ -0,0 +1,42 @@
+package Actividad9Estudiando;
+
+public class Libros extends Publicaciones implements Prestable {
+
+	//Constantes
+	private final static boolean NOPRESTADO = false;
+	
+	//Atributos
+	private boolean prestado;
+	
+	//Constructor
+	public Libros(String codigo, String titulo, int anioPublicacion) {
+		super(codigo, titulo, anioPublicacion);
+		this.prestado = NOPRESTADO;
+	}
+	
+	//Métodos de la interfaz
+	@Override
+	public void prestar() {
+		prestado = true;
+		
+	}
+
+	@Override
+	public void devolver() {
+		prestado = false;
+		
+	}
+
+	@Override
+	public boolean prestado() {
+		return prestado;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Libros [prestado=" + prestado + ", toString()=" + super.toString() + "]";
+	}
+
+	
+}
