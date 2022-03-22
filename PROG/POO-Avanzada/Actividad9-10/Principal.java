9d4952d7ae019fd205e80d767b1245eef3547740
Pablon03 <60820789+Pablon03@users.noreply.github.com>
2022-03-22 10:06:15 +0100

    Create Principal.java

---
+++ PROG/POO-Avanzada/Actividad9-10/Principal.java
@@ -0,0 +1,16 @@
+package Actividad9Estudiando;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Publicaciones listaPublicaciones[] = new Publicaciones[4];
+		listaPublicaciones[0] = new Libros("1685R", "Mktg Digital", 2021);
+		listaPublicaciones[1] = new Libros("1234Q", "El perro y el gato", 2015);
+		listaPublicaciones[2] = new Revistas("123456W", "Rompe la barrera del NO", 2010, 1);
+		listaPublicaciones[3] = new Revistas("68546P", "Vende como el mejor", 2020, 2);
+		
+		
+		
+	}
+}
