e451f50ae8d56c3d5ef37a61eeadbb998d3f6b46
Pablon03 <60820789+Pablon03@users.noreply.github.com>
2022-03-22 10:05:43 +0100

    Create Publicaciones.java

---
+++ PROG/POO-Avanzada/Actividad9-10/Publicaciones.java
@@ -0,0 +1,24 @@
+package Actividad9Estudiando;
+
+public class Publicaciones {
+
+	
+	//Atributos
+	private String codigo;
+	private String titulo;
+	private int anioPublicacion;
+	
+	//Constructor
+	public Publicaciones(String codigo, String titulo, int anioPublicacion) {
+		this.codigo = codigo;
+		this.titulo = titulo;
+		this.anioPublicacion = anioPublicacion;
+	}
+
+	@Override
+	public String toString() {
+		return "Publicaciones [codigo=" + codigo + ", titulo=" + titulo + ", anioPublicacion=" + anioPublicacion
+				+ "]";
+	}
+	
+}
