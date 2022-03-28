0c5a7657789106aa371a361525c4187e16ce1568
Alvaro Martin Crespo <101010208+AlvaroMartinCrespo@users.noreply.github.com>
2022-03-28 21:30:54 +0200

    Update AeropuertoPublico.java

--- PROG/Práctica 7/AeropuertoPublico.java
+++ PROG/Práctica 7/AeropuertoPublico.java
@@ -1,1 +1,35 @@
-a
+package Practica7;
+
+public class AeropuertoPublico extends Aeropuerto {
+
+	/*
+	 * Atributos
+	 */
+	private double subvencion;
+
+	/*
+	 * Constructores
+	 */
+	public AeropuertoPublico(String nombre, String ciudad, String pais) {
+		super(nombre, ciudad, pais);
+
+	}
+
+	public AeropuertoPublico(String nombre, String ciudad, String pais, double subvencion) {
+		super(nombre, ciudad, pais);
+		this.subvencion = subvencion;
+	}
+
+	public AeropuertoPublico(String nombre, String ciudad, String pais, Compañia[] compañia, double subvencion) {
+		super(nombre, ciudad, pais, compañia);
+		this.subvencion = subvencion;
+	}
+
+	/*
+	 * getters
+	 */
+
+	public double getSubvencion() {
+		return subvencion;
+	}
+}
