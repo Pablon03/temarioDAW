5638c7b9e0eae30139352733f7e32dc558639bac
Pablon03 <60820789+Pablon03@users.noreply.github.com>
2022-03-28 21:03:46 +0200

    Create Compañia.java

---
+++ PROG/Práctica 7/Compañia.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class Compañia {
+
+	//Atributos
+	private String nombre;
+	private Vuelo[] listaVuelo;
+	private int numVuelo;
+	
+	// Constructores
+	public void Compañia(String nombre, Vuelo[] vuelo) {
+		this.nombre = nombre;
+		this.listaVuelo = listaVuelo;
+		this.numVuelo = 0;
+	}
+	
+	public void Compañia(String nombre) {
+		this.nombre = nombre;
+		this.numVuelo = 0;
+	}
+	
+	public void insertarVuelo(Vuelo listaVuelo) {
+		this.listaVuelo[numVuelo] = listaVuelo;
+		numVuelo ++;
+	}
+
+	// Getters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public Vuelo getListaVuelo(int i) {
+		return listaVuelo[i];
+	}
+
+	public int getNumVuelo() {
+		return numVuelo;
+	}
+	
+	
+}
