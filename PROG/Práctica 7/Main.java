7d9ac9870a4586fb7b3c522d9f48d43a338ba97c
Pablon03 <60820789+Pablon03@users.noreply.github.com>
2022-03-29 21:05:25 +0200

    Update Main.java

--- PROG/Práctica 7/Main.java
+++ PROG/Práctica 7/Main.java
@@ -1,5 +1,3 @@
-package Practica7;
-
 import java.util.*;
 
 public class Main {
@@ -7,26 +5,31 @@ public class Main {
 	static Scanner entrada = new Scanner(System.in);
 
 	public static void main(String[] args) {
-
 		/*
 		 * Datos
 		 */
 		Aeropuerto[] listaAeropuertos = new Aeropuerto[6];
-		
+
 		AeropuertoPrivado apriv1 = new AeropuertoPrivado("AeropuertoPrivado1", "Ciudad1", "Pais1");
 		AeropuertoPrivado apriv2 = new AeropuertoPrivado("AeropuertoPrivado2", "Ciudad2", "Pais2");
 		AeropuertoPrivado apriv3 = new AeropuertoPrivado("AeropuertoPrivado3", "Ciudad3", "Pais3");
-		AeropuertoPublico apub1 = new AeropuertoPublico("AeropuertoPrivado4", "Ciudad4", "Pais4");
-		AeropuertoPublico apub2 = new AeropuertoPublico("AeropuertoPrivado5", "Ciudad5", "Pais5");
-		AeropuertoPublico apub3 = new AeropuertoPublico("AeropuertoPrivado6", "Ciudad6", "Pais6");
-		
-		listaAeropuertos[0]=apriv1;
-		listaAeropuertos[1]=apriv2;
-		listaAeropuertos[2]=apriv3;
-		listaAeropuertos[3]=apub1;
-		listaAeropuertos[4]=apub2;
-		listaAeropuertos[5]=apub3;
-				
+		AeropuertoPublico apub1 = new AeropuertoPublico("AeropuertoPrivado4", "Ciudad4", "Pais4", 1500);
+		AeropuertoPublico apub2 = new AeropuertoPublico("AeropuertoPrivado5", "Ciudad5", "Pais5", 2500);
+		AeropuertoPublico apub3 = new AeropuertoPublico("AeropuertoPrivado6", "Ciudad6", "Pais6", 3500);
+
+		listaAeropuertos[0] = apriv1;
+		listaAeropuertos[1] = apriv2;
+		listaAeropuertos[2] = apriv3;
+		listaAeropuertos[3] = apub1;
+		listaAeropuertos[4] = apub2;
+		listaAeropuertos[5] = apub3;
+
+		apriv1.insertarEmpresa("Microsoft");
+		apriv1.insertarEmpresa("Apple");
+		apriv2.insertarEmpresa("Samsung");
+		apriv2.insertarEmpresa("Oppo");
+		apriv3.insertarEmpresa("Medion");
+		apriv3.insertarEmpresa("Xiaomi");
 
 		/*
 		 * Variables
@@ -69,21 +72,27 @@ public class Main {
 
 	}
 
-	public static void tratarMenu(int accion, Aeropuerto[] listaAeropuerto) {
+	public static void tratarMenu(int accion, Aeropuerto[] listaAeropuertos) {
 
 		switch (accion) {
 
 		case 1:
-			consultarAeropuertos(listaAeropuerto);
+			consultarAeropuertos(listaAeropuertos);
 			break;
+			
 		case 2:
+			metodoDos(listaAeropuertos);
 			break;
+			
 		case 3:
 			break;
+			
 		case 4:
 			break;
+			
 		case 5:
 			break;
+			
 		case 6:
 			break;
 
@@ -100,38 +109,36 @@ public class Main {
 	 */
 
 	public static void consultarAeropuertos(Aeropuerto[] listaAeropuertos) {
-
 		System.out.println("Aeropuertos Privados");
-		
 		for(int i=0;i<listaAeropuertos.length;i++) {
-			
 			if(listaAeropuertos[i] instanceof AeropuertoPrivado) {
-				
 				System.out.println(listaAeropuertos[i].toString());
-				
-			}
-			
+			}	
 		}
-		
 		System.out.println();
 		System.out.println("Aeropuestos Publicos");
-		
 		for(int i=0;i<listaAeropuertos.length;i++) {
-			
 			if(listaAeropuertos[i] instanceof AeropuertoPublico) {
-				
 				System.out.println(listaAeropuertos[i].toString());
-				
 			}
-			
 		}
-		
 	}
-	
-	public static void listaCompañiasAeropuerto(Aeropuerto aeropuerto) {
-		
-		
-		
+
+	public static void metodoDos(Aeropuerto[] listaAeropuertos) {
+
+		System.out.println("//Aeropuertos Privados//");
+		for (int i = 0; i < listaAeropuertos.length; i++) {
+			if (listaAeropuertos[i] instanceof AeropuertoPrivado) {
+				System.out.println(((AeropuertoPrivado) listaAeropuertos[i]).toString());
+			}
+		}
+		System.out.println("");
+		System.out.println("//Aeropuertos Públicos//");
+		for (int i = 0; i < listaAeropuertos.length; i++) {
+			if (listaAeropuertos[i] instanceof AeropuertoPublico) {
+				System.out.println(((AeropuertoPublico) listaAeropuertos[i]).toString());
+			}
+		}
 	}
 
 }
